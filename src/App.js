6fee9028bcbfbf43943dd1a6259445ecc01408e9
Shy Ruparel <Shy@users.noreply.github.com>
2020-08-11 12:13:26 -0400

    Added progress from First Livestream
    Check out the Livestream at: https://www.youtube.com/watch?v=dUWFiEuZmw8&feature=youtu.be

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const query = `query{
+  food (id: "5dUVVIfVC9Ze3Z4r4LedoR") {
+    foodName
+    picture{
+      url(transform: {width:600, cornerRadius: 1000, format:PNG} )
+      description
+    }
+  }
+}`;
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(()=> {
+    window.fetch('https://graphql.contentful.com/content/v1/spaces/69qc09r1b31n/?access_token=-1dsnun-jy-tEy1ZpRN6izl5CBr0EKv3yuRAzqqzpLc',
+    {headers:{
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({query}),
+    })
+    .then(response => response.json())
+    .then(json => setData(json.data));
+  },[]);
+  if (!data) return <span>Loading :(</span>;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={data.food.picture.url} className="App-logo" alt={data.food.picture.description} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.food.foodName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
